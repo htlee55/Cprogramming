fb631f6881c51259f246c9e0207c2ba4212a4f51
htlee55 <htlee55@naver.com>
2026-03-31 11:00:29 +0900

    Create while.c

---
+++ chapter3/while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for(int i=0;i<10;i++){
+		printf("Hello World!\n");
+	}
+	
+	i = 0;
+	while(i<10)
+	{
+		printf("Hello World! \n");
+		i++;
+	}
+
+	int i = 1;
+	do {
+		printf("Hello World! %d\n",i++
+	} while(i<=10);
+
+	return 0;
+}
