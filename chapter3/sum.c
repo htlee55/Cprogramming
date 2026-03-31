401b3146b5d2cc8b204793f6e2d189e38c99a8c1
htlee55 <htlee55@naver.com>
2026-03-31 10:49:24 +0900

    Add sum.c to calculate sum of first 10 integers
    Implement a program to calculate the sum of integers from 1 to 10.

---
+++ chapter3/sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;	
+	for(i = 1; i <= 10; i++)	// i는 1부터 10까지 증가
+		sum += i;		// sum = sum + i;와 같음
+
+	printf("1부터 10까지의 정수의 합= %d\n",sum);
+	return 0;
+}
