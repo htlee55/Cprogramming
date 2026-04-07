093fa53e8fb05273022dd73a595cf6e62ebde14c
htlee55 <htlee55@naver.com>
2026-04-07 10:58:47 +0900

    Update 4_arithmatic.c

--- chapter5/4_arithmatic.c
+++ chapter5/4_arithmatic.c
@@ -1,17 +1,19 @@
+#include <stdio.h>
+
 int add(int num1, int num2);
-void o(int num);
+void p(int num);
 
-int main(){
-  int num;
-  num = add(num,3);
-  p(num);
-  return 0;
+int main() {
+	int num = 2;
+	num = add(num, 3);
+	p(num);
+	return 0;
 }
 
-int add(int num1, int num2){
-  return num1 + num2;
+int add(int num1, int num2) {
+	return num1 + num2;
 }
 
-void p(int num){
-  printf("number is %d.\n", num);
+void p(int num) {
+	printf("number is %d.\n", num);
 }
