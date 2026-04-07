9d1648e5401e0faa7752feacbbd46cfc082af071
htlee55 <htlee55@naver.com>
2026-04-07 10:25:38 +0900

    Update 3_with_return.c

--- chapter5/3_with_return.c
+++ chapter5/3_with_return.c
@@ -1,16 +1,18 @@
+
 #include <stdio.h>
 
-void function_with_return();	// 반환값이 있는 함수를 선언합니다.
+int function_with_return();	// 반환값이 있는 함수를 선언합니다.
 
 int main(void)
 {
-	int return = function_with_return();	// 반환값이 있는 함수를 호출합니다.
-  printf("%d", ret);
+	int ret = function_with_return();	// 반환값이 있는 함수를 호출합니다.
+	printf("%d", ret);
 	return 0;
 }
 
-void function_with_return()	//한글 주석으로 부탁합니다.
+int function_with_return()	//한글 주석으로 부탁합니다.
 {
-	printf("이 함수는 반환값이 있는 함수입니다.\n");	
-  return 10;
+	printf("이 함수는 반환값이 있는 함수입니다.\n");
+	return 10;
 }
+
