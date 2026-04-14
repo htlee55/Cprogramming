4f3ed2150b74c500933669d49e384ab5032d255d
htlee55 <htlee55@naver.com>
2026-04-14 13:26:36 +0900

    Update 3_cal_scores.c

--- chapter6/3_cal_scores.c
+++ chapter6/3_cal_scores.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #define STUDENTS 5
 
-
-int scores[20]
-
-
-scanf();
-
-average
+int main(void){
+  int score[STUDENTS];
+  int sum = 0;
+  int average;
+  for(int i=0;i<STUDENTS;i++){
+    printf("학생들의 성적을 입력하시오: ");
+    scanf("%d", &score[i]);
+  }
+  for(int i=0;i<STUDENTS;i++)
+    sum += s[i];
+  average = sum/STUDENTS;
+  printf("성적 평균=%d\n",avarge);
+  return 0;
+}
 
