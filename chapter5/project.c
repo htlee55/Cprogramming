868dff01937dfad3637a6ca49ff3b4c4a8b41189
htlee55 <htlee55@naver.com>
2026-04-14 12:53:39 +0900

    Add basic quiz game functionality in project.c
    Implement a basic quiz game that generates random multiplication questions and evaluates user answers.

---
+++ chapter5/project.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <time.h> // 헤더 파일 추가
+#include <stdlib.h> // 헤더 파일 추가 
+
+int getRandomNumber(int level);
+void showQuestion(int level, int num1, int num2);
+void success();
+void fail();
+
+int main(void)
+{
+	// 난수 초기화
+	// 변수 선언 및 초기화(맞힌 문제 개수)
+	for (문제 5개 반복)
+	{
+		// 문제 출제 x * y = ?
+		// 문제 출력
+		int answer = -1;
+		// 답 입력
+		if (answer == -1) // 입력값이 -1일 때
+		{
+    		// break;
+			exit(0);
+		}
+		else if ( 정답을 맞혔을 때 )
+		{
+			//정답 출력
+			// 맞힌 문제 개수 증가
+		}
+		else // 정답을 못 맞혔을 때
+		{
+			//실패 출력
+		}
+	}
+	비밀번호 맞춘 갯수 출력
+	return 0;
+}
