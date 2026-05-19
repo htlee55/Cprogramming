e8998e8238d88b7d4d1a9afc3914b93e70970e09
htlee55 <htlee55@naver.com>
2026-05-19 12:42:39 +0900

    Update main.c

--- SDL/main.c
+++ SDL/main.c
@@ -1,37 +1,36 @@
-//spaceGame1
 #include <stdio.h> 
 #include <SDL.h>
-#include "header.h"
+#include "engine.h"
 
 // 전역 변수
-SDL_Window* window;
-SDL_Renderer* renderer;
+SDL_Window* window;                 // SDL 창을 가리키는 포인터
+SDL_Renderer* renderer;             // SDL 렌더러를 가리키는 포인터
 
-int main(int argc, char* argv[]) {
-
+int main(int argc, char** argv)
+{
     if (!engine_init()) {
         printf("Error initializing engine: %s\n", SDL_GetError());
         return 1;
     }
+    SDL_Event event;
+    int quit = 0;
 
-    initGame();
-
-    while (running) {           // Game Loop
-        SDL_Event event;
+    while (!quit) {
+        // 이벤트 처리
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT)
-                running = false;
-            handleInput(&event);
+            if (event.type == SDL_QUIT) {// 윈도우의 x 표시를 누르면
+                quit = 1;
+            }
+            // Pass events on queue
+            engine_handle_event(&event);
         }
-        // update the game
-        updateGame();
-        // render the game
-        renderGame();
-        // update the window
-        SDL_Delay(100);
+        // Update the engine
+        engine_update();
+        // Render the engine 
+        engine_render();
     }
-    // cleanup the game
-    cleanupGame();
+    // Clean up the engine
+    engine_cleanup();
 
     return 0;
 }
